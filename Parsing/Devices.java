bcce26336ff3ad6cf27bf33911c325c9308d984e
felix2000jp <felix2000jp@gmail.com>
2021-12-05 22:53:42 +0000

    Underlay

---
+++ Parsing/Devices.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Devices {
+    private HashMap<Integer, Device> devices;
+
+    // CONSTRUCTORS
+    public Devices() {
+        this.devices = new HashMap<Integer, Device>();
+    }
+
+
+    // GETTERS
+    public HashMap<Integer, Device> getDevices() {
+        return devices;
+    }
+
+    public ArrayList<Device> getDevicesList() {
+        return new ArrayList<Device>(devices.values());
+    }
+
+    public Device getDevice(int id) {
+        return this.devices.get(id);
+    }
+
+
+    // SETTERS
+    public void setDevices(HashMap<Integer, Device> devices) {
+        this.devices = devices;
+    }
+
+    public void addDevices(HashMap<Integer, Device> devices) {
+        this.devices.putAll(devices);
+    }
+
+    public void addDevice(Device device) {
+        this.devices.put(device.getId(), device);
+    }
+
+
+    // OTHER
+    public Boolean contains(int id) {
+        return this.devices.keySet().contains(id);
+    }
+
+    public void print() {
+        System.out.println("DEVICES");
+        for(Device device : this.devices.values()) {
+            device.print();
+        }
+    }
+}
---
+++ UDPServer.java
@@ -0,0 +1,51 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+    private DatagramSocket datagramSocket;
+    private byte[] buffer; 
+
+    private int port;
+    private InetAddress inetAddress;
+
+    // Contructor
+    public UDPServer() throws Exception{
+        port = 5000;
+        inetAddress = InetAddress.getByName("localhost"); // CHANGE
+        this.datagramSocket = new DatagramSocket(this.port, this.inetAddress); 
+    }
+
+
+    // Treats message received
+    private void treatMessage(DatagramPacket datagramPacket) {
+        String message = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+        System.out.println("Message from Client: " + message);
+    }
+
+    // Receives Message
+    private DatagramPacket receive() {
+        // Inicializa o packet
+        DatagramPacket datagramPacket = null;
+        try {
+            // O packet é preenchido com informação
+            datagramPacket = new DatagramPacket(this.buffer, this.buffer.length);   
+            this.datagramSocket.receive(datagramPacket);
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("UDPServer receive() NOT YO");
+        }
+        return datagramPacket;
+    }
+
+    
+    public void run(){
+        while(true) {
+            this.buffer = new byte[256];
+            DatagramPacket datagramPacket = receive();
+            treatMessage(datagramPacket);
+            new Thread( new UDPWorker(this.datagramSocket, datagramPacket, this.buffer) ).start();
+        }
+    }
+}
